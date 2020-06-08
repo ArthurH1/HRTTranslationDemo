04b12211058f806949703eb34ea7d32c8aee73ce
Arthur Huber <huber_arthur@protonmail.com>
2020-06-08 19:18:44 +0200

    Fix login script

--- ibmcloud_stuff/login_and_select_namespace_context.sh
+++ ibmcloud_stuff/login_and_select_namespace_context.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ibmcloud login ibmcloud login -a https://cloud.ibm.com -r eu-gb
+ibmcloud login -a https://cloud.ibm.com -r eu-gb
 ibmcloud target -g Default
 ibmcloud target -r eu-de
 ibmcloud fn property set --namespace 61a8ba7e-a114-4441-b4e1-2365f8da273e
